f6a45309d48fc2bc52b8ee627475d1de3306ae0e
Vineeth Viswan <vineeth.viswan@live.com>
2024-11-05 23:06:07 -0400

    Day-1 : Getting started

---
+++ Day-1/index.js
@@ -0,0 +1,25 @@
+
+/*----------------------------------------------------------------------------------------------------------------------------------
+Day 1 - Node.JS Download and Installation
+
+Goals
+    -   Set up your development environment and install necessary software
+    -   Write your first "Hello World" program
+Tasks
+    1. Install Node.js and npm
+        -   Download and install Node.js from the official website https://nodejs.org
+        -   Verify the installation by running `node -v` and `npm -v` in your terminal to check the versions.
+    2. Install a Code Editor    
+        -   Download and install a code editor if you don't have one.
+        -   We recommend: Visual Studio Code
+    3. Set Up the Project Directory    
+        -   Create a new directory for your project
+        -   Open your terminal and navigate to the project directory
+    4. Initialize a Node.js Project
+        -   Run `npm init -y` in your terminal to create a `package.json` file with default settings.
+    5. Write Your First JavaScript Program
+        -   Create a file named `index.js` in your project directory and write a simple program that outputs "Hello World!" in your console
+
+----------------------------------------------------------------------------------------------------------------------------------*/
+
+console.log("Hello World!");
