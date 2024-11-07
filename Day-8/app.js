b0700cb886522f47160ea1bf0bc0f1f02533b8ec
Vineeth Viswan <vineeth.viswan@live.com>
2024-11-06 23:17:19 -0400

    Day-8 : Updated task description

--- Day-8/app.js
+++ Day-8/app.js
@@ -1,3 +1,7 @@
+/*------------------------------------------------------------
+ Day 8 - Express Basics
+         Set up an Express application and create routes
+--------------------------------------------------------------*/
 
 const express = require('express');
 const app = express();
